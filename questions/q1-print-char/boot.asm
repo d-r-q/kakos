4397a86e6adb8d77aad17b46f33b390a8ffebb71
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2015-12-06 14:06:18 +0600

    Fix q1-print-char code

--- questions/q1-print-char/boot.asm
+++ questions/q1-print-char/boot.asm
@@ -1,26 +1,19 @@
 ; Try to print char using different ways to specify it's address
   mov ah, 0x0e
   
-  mov bp, 0x8000
-  mov sp, sp
-
-  push 'A'
-  push 'B'
-  push 'C'
-
-  pop bx
-  mov al, bl
+  mov al, the_secret
   int 0x10
 
-  pop bx
-  mov al, bl
+  mov al, [the_secret]
   int 0x10
 
-  mov al, [0x7ffe]
+  mov bx, the_secret
+  add bx, 0x7c00
+  mov al, [bx]
   int 0x10
 
-;  mov al, [0x7c09]
-;  int 0x10
+  mov al, [0x7c1d]
+  int 0x10
 
   jmp $
 
